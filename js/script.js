f77ccd59d342fb1a7a76bc67981f95556a2f036d
Ilse Gaertner <joachimpruegl@gmail.com>
2023-06-10 10:47:24 +0200

    Refactor Code

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-let li = $('<li></li>');
+/*let li = $('<li></li>');
 li.append('<li>some item</li>');
-let inputValue = $('#input').val;
-let text = $
+let inputValue = $('input').val;
+let text = $('inputValue');
 
+*/
 // try again________________
 
 //1
 
-let li = $('<li id="input"></li>');
-let inputValue = $('#input').val;
-$ ('body').append(li);  
+let li = $('<li></li>');
+li.append('<li></li>');
+let inputValue = $('input').val;
+//$('inputValue').append('input').val;
+let text = $('<span>').text(inputValue);
+li.append(text);
 
 if (inputValue === '') {
     alert("You must write something!");
   } else {
-    let list = $('#list');
+    let list = $('list');
     list.appendChild(li);
   }
+
 
 //2
 
@@ -32,7 +37,7 @@ deleteButton.append(document.createTextNode('X'));
 ___________________
 // JQuery
 
-
+/*
 $('#list')
 let list = $('#list');
 let li = $('<li></li>');
@@ -40,7 +45,7 @@ li.addClass("strike");
 list.append(li); //or: list.append("<li>list something</li>");
 
 
-/* crossing out by clicking on it */
+crossing out by clicking on it 
 li.on("click", function() {
     li.addClass("strike");
 });
@@ -51,5 +56,6 @@ let inputValue = $('#input').val;
 
 li.append(inputValue);
 
-/* adding class to display:none */
+adding class to display:none 
 li.addClass("delete");
+*/
